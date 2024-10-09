a5f1a354d50bc2552442f19b1341a2536e36e6e2
Grace <grace@404.si>
2024-10-09 17:44:19 +0200

    prvi popravek

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,6 @@
 	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
 		Hello World, Hello Grace! Hello Miha!
 	</h1>
+	<p class="text-violet-900 text-4xl">Tukaj pa je še nekaj besedila.</p>
 	<img class="rounded-md w-96" src="art.webp" />
 </div>
